ffadbc3a4508c64084bb24288bfe5e6a7a79607b
RAFA1211 <rafael.angarita.raar@gmail.com>
2021-07-23 07:39:35 -0500

    corrección Sintaxis_JavaScript_02_07_2021

---
+++ main.js
@@ -0,0 +1,10 @@
+
+nombreString = `Rafael Angel Angarita Rodríguez`;
+edadNumber = 47;
+soyProfesor = false;
+
+
+console.log(`Cual es mi nombre?: `, nombreString);
+console.log(`Cual es mi edad?:   `, edadNumber);
+console.log(`Soy Profesor:   `, soyProfesor)
+
